bbfc0581aa806b3a6326d22886e4cfbffea467f8
Pamasich <pamasich@gmail.com>
2020-04-30 01:55:18 +0200

    Filled index.js with code from an example bot tutorial, and put in my bot's token

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Logged in as ${client.user.tag}');
+})
+
+client.on('message', msg => {
+    if (message.author.bot) return;
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login('NzA1MjAzMDA1MzEzNzc3Nzk2.XqoSCw.i5fufSus3kRazOgb7l5sgvL1xgM');
