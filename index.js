ade44361ae28ce55465589bd0acb65fa80415b10
Pamasich <pamasich@gmail.com>
2020-04-30 02:05:41 +0200

    Renamed client to bot

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
 /*client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}');
