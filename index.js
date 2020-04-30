93307aa650cd43f871472f960a9cae942ea233cc
Pamasich <pamasich@gmail.com>
2020-04-30 02:30:30 +0200

    Now mapping all commands to lowercase initially, so that doesn't have to be done each comparison

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('message', msg => {
-    const msgParts = msg.content.split(" ");
-    if (msgParts[0].toLowerCase() === 'shamiko') {
-        switch (msgParts[1].toLowerCase()) {
+    const msgParts = msg.content
+                        .split(' ')
+                        .map(cmd => cmd.toLowerCase());
+
+    if (msgParts[0] === 'shamiko') {
+        switch (msgParts[1]) {
             case 'ping':
                 msg.reply('Pong!');
                 break;
