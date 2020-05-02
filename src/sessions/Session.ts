2308bc2707c2c422bae3e7ecb6ae9f98d0b57b65
Pamasich <pamasich@gmail.com>
2020-05-02 21:50:05 +0200

    Added doc comments for session class

--- src/sessions/Session.ts
+++ src/sessions/Session.ts
@@ -1,25 +1,42 @@
 import { User } from 'discord.js';
 
+/**
+    Represents an ongoing session, which is associated with a user and a topic.
+*/
 export class Session {
     private lastUpdate: Date = new Date();
     private ctx: Map<string, string> = new Map();
     readonly user: User;
     readonly topic: string;
 
-    getLastUpdate(): Date {
-        return this.lastUpdate;
-    }
-
     constructor(user: User, topic: string) {
         this.user = user;
         this.topic = topic;
     }
 
+    /**
+        Retrieves the timestamp of when the session got last updated.
+        @returns The timestamp
+    */
+    getLastUpdate(): Date {
+        return this.lastUpdate;
+    }
+
+    /**
+        Stores a value in the session.
+        @param key The key to update/add
+        @param value The value to save
+    */
     set(key: string, value: string): void {
         this.ctx.set(key, value);
         this.lastUpdate = new Date();
     }
 
+    /**
+        Retrieves the value of a key.
+        @param key The key to retrieve
+        @returns The value of the key
+    */
     get(key: string): string | undefined {
         return this.ctx.get(key);
     }
